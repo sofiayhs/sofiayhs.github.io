0ec0171e6c101c855f2b6b757fd85365af47518c
sofiayhs <hamratsiysofia@gmail.com>
2023-12-10 12:40:44 +0100

    .

--- pf.js
+++ pf.js
@@ -1,9 +1,11 @@
 const toggleBtn = document.getElementsByClassName('btn-toggle')
-const navbar = document.querySelectorAll('.main-navbar ul')
+const menuList = document.querySelectorAll('.main-navbar ul')
+const navbar = document.querySelector('.main-navbar')
 
 toggleBtn[0].addEventListener("click", toggleMenu)
 
 function toggleMenu(){
-    navbar[0].classList.toggle("visible")
+    menuList[0].classList.toggle("visible")
+    navbar.classList.toggle("background")
 }
 console.log(navbar)
