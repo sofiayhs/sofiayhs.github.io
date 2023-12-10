a49bbf6df05332d35f0be93b8b808a6db18cdb26
sofiayhs <hamratsiysofia@gmail.com>
2023-12-10 17:17:21 +0100

    form

--- pf.js
+++ pf.js
@@ -1,11 +1,30 @@
-const toggleBtn = document.getElementsByClassName('btn-toggle')
-const menuList = document.querySelectorAll('.main-navbar ul')
-const navbar = document.querySelector('.main-navbar')
+const toggleBtn = document.querySelector(".btn-toggle");
+const menuList = document.querySelectorAll(".main-navbar ul");
+const navbar = document.querySelector(".main-navbar");
 
-toggleBtn[0].addEventListener("click", toggleMenu)
+toggleBtn.addEventListener("click", toggleMenu);
 
-function toggleMenu(){
-    menuList[0].classList.toggle("visible")
-    navbar.classList.toggle("background")
+function toggleMenu() {
+  menuList[0].classList.toggle("visible");
+  navbar.classList.toggle("background");
+  toggleBtn.classList.toggle("bi-list");
+  toggleBtn.classList.toggle("bi-x-lg");
 }
-console.log(navbar)
+
+function sendEmail() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  let body = "Name: " + name + 
+  " <br /> From: " + email + 
+  " <br />Message: " + message ;
+
+  Email.send({
+    SecureToken: "0f4bb1f6-1fb6-4c30-9912-d77ef4d3f7bf",
+    To: "hamratsiysofia@gmail.com",
+    From: "hamratsiysofia@gmail.com",
+    Subject: "msg from Contact Form (portfolio)",
+    Body: body,
+  }).then((message) => alert(message));
+}
